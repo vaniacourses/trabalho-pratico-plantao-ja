98cae9bc44f07cf8ab69bd0c552fcf0dc2883571
Joao Marcos <martins_joao@id.uff.br>
2026-05-28 10:51:03 -0300

    feat/creating project and packages

---
+++ backend/plantao-ja/src/test/java/br/com/plantao/plantao_ja/PlantaoJaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.plantao.plantao_ja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantaoJaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
