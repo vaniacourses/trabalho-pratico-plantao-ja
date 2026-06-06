db6838d8627f22615274a3f94144dfd635bdebe6
romulofgoes <romulofgoes@proton.me>
2026-06-05 22:42:29 -0300

    reat: login submit handler requisitando servidor teste

--- frontend/plantao-ja/plantao-ja/src/pages/LoginPage.tsx
+++ frontend/plantao-ja/plantao-ja/src/pages/LoginPage.tsx
@@ -8,10 +8,34 @@ interface LoginPageProps {
 const LoginPage: React.FC<LoginPageProps> = ({ title = "Login"}) => {
     const [text, setText] = useState(""); //1.Define o estado
     const [password, setPassword] = useState("")
-    const handleSubmit = (e: React.SubmitEvent) => {
+    const [status, setStatus] = useState("");   
+    const handleSubmit = async (e: React.SubmitEvent) => {
         e.preventDefault(); // Impede a página de recarregar e quebrar o React
         console.log("Dados enviados:", { text, password });
         // Aqui você faz a chamada de login ou usa o React Router
+        setStatus("Submitting...");
+
+        try {
+        const response = await fetch("http://localhost:8000/", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({text, password}),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            setStatus(`Success: ${data.message}`);
+            setText(''); 
+            setPassword("");// Clear form
+        } else {
+            setStatus("Submission failed. Server returned an error.");
+        }
+        } catch (error) {
+            console.error("Connection error:", error);
+            setStatus("Could not connect to the backend server.");
+        }
     };
     return (
         <>
@@ -20,12 +44,13 @@ const LoginPage: React.FC<LoginPageProps> = ({ title = "Login"}) => {
             </header>
             <div>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Usuário" id="user-id" value={text} 
+                    <input type="text" name="nome" placeholder="Usuário" id="user-id" value={text} 
                     onChange={(e) =>setText(e.target.value)}/>
-                    <input type="password" placeholder="Senha" id="password" value={password} 
+                    <input type="password" name="senha" placeholder="Senha" id="password" value={password} 
                     onChange={(e) =>setPassword(e.target.value)}/>
                     <button type='submit'>Submit Form</button>
                 </form>
+                {status && <p>{status}</p>}
             </div>
         </>
     );
