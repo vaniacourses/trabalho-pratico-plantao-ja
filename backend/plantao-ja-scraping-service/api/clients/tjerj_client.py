7623ad809fcf6a3c13d0403f693c65d4f06d569a
Pedro Ribeiro Filho <pyyyter@hotmail.com>
2026-06-10 06:16:18 -0300

    Feat : Adding CPF query

--- backend/plantao-ja-scraping-service/api/clients/tjerj_client.py
+++ backend/plantao-ja-scraping-service/api/clients/tjerj_client.py
@@ -1,7 +1,7 @@
 import requests
 
-#usando o inspector, descobri que o TJERJ tem uma API interna que é usada para buscar processos por nome de parte. A URL é a seguinte:
-BASE_URL = "https://www3.tjrj.jus.br/consultaprocessual/api/processos/por-nome-parte"
+#usando o inspector, descobri que o TJERJ tem um endpoint HTTP utilizado pelo front-end público do site que é usada para buscar processos. A URL é a seguinte:
+BASE_URL = "https://www3.tjrj.jus.br/consultaprocessual/api/processos"
 
 def buscar_processos_por_nome(nome: str):
     payload = {
@@ -25,6 +25,46 @@ def buscar_processos_por_nome(nome: str):
         "radio": "1"
     }
 
-    response = requests.post(BASE_URL, json=payload, timeout=30)
-    response.raise_for_status()
-    return response.json()
+    try:
+        response = requests.post(
+            f"{BASE_URL}/por-nome-parte",
+            json=payload,
+            timeout=30
+        )
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        return {"erro": "Falha ao consultar TJERJ", "detalhe": str(e)}
+
+def buscar_processos_por_cpf(cpf: str):
+    payload = {
+        "anoInicial": 2025,
+        "anoFinal": 2026,
+        "origem": "1",
+        "codCom": None,
+        "codComp": None,
+        "cpfcnpj": cpf,
+        "comarca": "201",
+        "competencia": "01",
+        "totalProcessoPesquisa": 300,
+        "tipoConsulta": "publica",
+        "isPortalDeServicos": 1,
+        "isPortal": "S",
+        "pIsProcAtivos": "N",
+        "secao": "RJ",
+        "tipoSegundaInstancia": "1",
+        "validarSecao": True,
+        "aba": "cpfcnpj",
+        "radio": "1"
+    }
+
+    try:
+        response = requests.post(
+            f"{BASE_URL}/por-cpf-cnpj",
+            json=payload,
+            timeout=30
+        )
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        return {"erro": "Falha ao consultar TJERJ", "detalhe": str(e)}
