efcc69b9e3893c15c961652ce03c372c008b59ce
Joao Marcos <martins_joao@id.uff.br>
2026-06-16 21:25:09 -0300

    adding gateway project

---
+++ backend/gateway/src/test/java/com/plantao_ja/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantao_ja.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
